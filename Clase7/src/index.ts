73012b9638bf52bf027ddd3189928530a915da4e
WilliamAvilaArmijos <williamavilaarmijos@hotmail.com>
2022-06-09 18:40:33 -0500

    Add files via upload

---
+++ Clase7/src/index.ts
@@ -0,0 +1,56 @@
+const newUID = (): string => Math.random().toString(36).slice(2)
+interface Producto{
+    id: string;
+    nombreProducto: string;
+    precio: number;
+}
+
+interface productoVenta extends Producto{
+    cantidad: number,
+    subtotal: number,
+    total: number
+}
+
+const productoArroz: productoVenta = {
+    id: newUID(),
+    nombreProducto: "Arroz",
+    precio: 30,
+    cantidad: 2,
+    subtotal: 52.80,
+    total: 60
+}
+
+const productoAtun: productoVenta = {
+    id: newUID(),
+    nombreProducto: "Atun",
+    precio: 2,
+    cantidad: 2,
+    subtotal: 52.80,
+    total: 60
+}
+
+class Factura{
+    prod: productoVenta[];
+    constructor(){
+        this.prod = [];
+    }
+    agregarProductos(produ: productoVenta){
+        produ.total=totalpro;
+        this.prod.push(produ);
+    }
+
+    calcularSubtotal(produ: productoVenta){
+        let totalpro= produ.precio*produ.cantidad
+        return totalpro
+    }
+
+}
+
+const factura = new Factura();
+factura.agregarProductos(productoArroz)
+factura.agregarProductos(productoAtun)
+
+console.log("              Factura")
+console.log("Cantidad" +" "+  "Nombre" +" "+  "Precio" +" "+  "Subtotal" +" "+  "Total")
+console.log(productoArroz.cantidad+ "         " +productoArroz.nombreProducto + "   " + productoArroz.precio+ "     " +productoArroz.subtotal+ "      " +productoArroz.total)
+console.log(productoAtun.cantidad+ "         " +productoAtun.nombreProducto + "   " + productoAtun.precio+ "     " +productoAtun.subtotal+ "      " +productoAtun.total)
